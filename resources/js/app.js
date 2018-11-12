730c8c46f7f1888729f4a151be36865d749f5bef
Daniele Del Giudice <dunox1@gmail.com>
2018-11-12 21:57:33 +0100

    Switched client jwt storage to prefer using cookies over localStorage.

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,7 +35,8 @@ Vue.router = router;
 Vue.use(require("@websanova/vue-auth"), {
     auth: require("@websanova/vue-auth/drivers/auth/bearer.js"),
     http: require("@websanova/vue-auth/drivers/http/axios.1.x.js"),
-    router: require("@websanova/vue-auth/drivers/router/vue-router.2.x.js")
+    router: require("@websanova/vue-auth/drivers/router/vue-router.2.x.js"),
+    tokenStore: ['cookie', 'localStorage']
 });
 
 // Global filters
