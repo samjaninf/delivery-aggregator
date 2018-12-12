80398209627b1d7d5a6d47e20a8b8cd10fee3f77
Daniele Del Giudice <dunox1@gmail.com>
2018-12-12 09:49:30 +0100

    Added js/css cache buster solution with laravel-mix.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,11 +13,11 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz"
             crossorigin="anonymous">
 
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
         <div id="app">
         </div>
-        <script src="/js/app.js"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
