152204dd67b4917471f96b9324840ace1a407a50
Daniele Del Giudice <dunox1@gmail.com>
2019-02-13 10:41:07 +0100

    Removed automatic printReceipt to prevent an issue with the android app.

--- resources/js/components/OrdersList.vue
+++ resources/js/components/OrdersList.vue
@@ -186,7 +186,6 @@ export default {
           .post(`stores/${this.activeStore.code}/orders/${order.number}/seen`)
           .then(() => {
             order.seen = true;
-            this.printReceipt(order);
           })
           .catch(e => {
             console.error(e);
@@ -198,13 +197,12 @@ export default {
       const header = `<center><big><bold>${storeName}<br><br><medium2><normal><left>Prodotti<br>`;
       const items = order.items
         .map(i => {
-          const title = `<medium1><normal><left>${i.quantity} x ${
-            i.name
-          }<right>  ${i.total.toFixed(2)}E<br>`;
+          const title = `<medium1><normal><left>${i.quantity} x ${i.name}<br>`;
           const addons = Object.keys(i.meta)
             .map(k => `<small>${k}<br><small>${i.meta[k]}<br>`)
             .join("");
-          return `${title}${addons}<br>`;
+          const price = "<small>${i.total.toFixed(2)} E";
+          return `${title}${addons}${price}<br>`;
         })
         .join("");
       const footer = "<cut>";
