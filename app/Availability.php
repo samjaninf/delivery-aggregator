4d7a334ae414d5f23d95381736078b2469d2b392
Daniele Del Giudice <dunox1@gmail.com>
2019-07-18 13:18:59 +0200

    Fixed issue with user deletion

--- app/Availability.php
+++ app/Availability.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Availability extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'type',
         'start',
